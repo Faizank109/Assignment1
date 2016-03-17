e1ac53ae8843f7b95a58a88487fa4d3012b54e98
Faizan <FaizanKhalid_cyberone321@hotmail.com>
2016-03-17 18:40:27 +0500

    assignment v1

---
+++ DOB/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Faizan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        
+        {
+            int siblings;
+            Console.WriteLine("Enter number of siblings :");
+            DateTime[] dat = new DateTime[100];
+            DateTime now = DateTime.Now;
+            siblings = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <=siblings; i++)
+            {
+                Console.WriteLine("Enter DOB of sibling"+i+" :");
+                dat[i] = Convert.ToDateTime(Console.ReadLine());
+            }
+            for (int i = 1; i <=siblings; i++)
+            {
+                TimeSpan total = now.Subtract(dat[i]);
+                int TotalDays = total.Days;
+                int TotalYears = TotalDays / 365;
+                int TotalMonths = (TotalDays % 365) / 30;
+                int TotalDayss = (TotalDays % 365) % 30;
+                Console.WriteLine("Age Of Sibling :"+i+" :"  +TotalYears+" Years"+ TotalMonths+ " Months"+TotalDayss+ "Days");
+
+                Console.Beep();
+                Console.Beep();
+                
+            }
+
+            
+            Console.ReadLine();
+        }
+    }
+}
