629dd59e83447784ca413995f6989088d6c5052a
Faizank109 <FaizanKhalid_cyberone321@hotmail.com>
2016-04-10 16:28:59 +0500

    Added files via upload

---
+++ ConsoleApplication8/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[5];
+            Console.WriteLine("Enter Values in Array");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine("Elements of Array are {0}", arr[i]);
+                Console.Read();
+            }
+        }
+    }
+}
