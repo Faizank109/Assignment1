7c909fc4bc83bd118f1ace695fe4e110d1c0decb
Faizank109 <FaizanKhalid_cyberone321@hotmail.com>
2016-04-10 16:29:32 +0500

    Added files via upload

---
+++ ConsoleApplication10/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "faizan khalid";
+            foreach (char c in name)
+            {
+                Console.WriteLine(c);
+            }
+            Console.Read();
+        }
+    }
+}
