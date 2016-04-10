447b0831ea3bbc37481cd2a18a5dfbe23dac5ba6
Faizank109 <FaizanKhalid_cyberone321@hotmail.com>
2016-04-10 16:27:05 +0500

    Added files via upload

---
+++ ConsoleApplication7/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication7
+{
+    delegate void del();
+    class Program
+    {
+
+
+
+        class student
+        {
+            public void test()
+            {
+                Console.WriteLine("iam in student class ");
+            }
+            static void Main(String[] args)
+            {
+                student obj = new student();
+                del delll = new del(obj.test);
+                delll();
+                
+                Console.Read();
+
+            }
+
+        }
+
+    }
+}
